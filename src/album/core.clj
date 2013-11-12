0365cf3259269af19c98240e344e5e62fe884ce0
Christian Berg <berg.christian@gmail.com>
2013-11-12 20:42:44 +0100

    Inital commit of new template project
    [finishes #68591172]

---
+++ src/album/core.clj
@@ -0,0 +1,6 @@
+(ns album.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/album/core_test.clj
@@ -0,0 +1,7 @@
+(ns album.core-test
+  (:require [clojure.test :refer :all]
+            [album.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
