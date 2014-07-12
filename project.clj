bb6a4053e77a6096136412d20471806039ad3117
Christian Berg <berg.christian@gmail.com>
2014-07-12 23:09:19 +0200

    Update dependency: fs

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.7.0"]
-                 [me.raynes/fs "1.4.5"]
+                 [me.raynes/fs "1.4.6"]
                  [com.drewnoakes/metadata-extractor "2.6.2"]])
