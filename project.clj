b011eb881f2275b6e3384b5e9bae77e614b0dc81
Christian Berg <berg.christian@gmail.com>
2014-08-16 23:44:01 +0200

    Update dependency versions.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.7.0"]
+                 [clj-time "0.8.0"]
                  [me.raynes/fs "1.4.6"]
                  [com.drewnoakes/metadata-extractor "2.6.2"]])
