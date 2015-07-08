5ce7c702e8d0666df953da300b86c33dbbc15125
Christian Berg <berg.christian@gmail.com>
2015-07-08 21:13:07 +0200

    Update dependencies: Clojure 1.7, clj-time 0.10

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-time "0.10.0"]
                  [me.raynes/fs "1.4.6"]
                  [com.drewnoakes/metadata-extractor "2.8.1"]])
